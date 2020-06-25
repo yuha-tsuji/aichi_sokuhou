ff6e99f5db42d116b87da44fe0ba5a3a232b08fc
yuha-tsuji <yuga7517@gmail.com>
2020-06-25 21:48:55 +0900

    Add title

---
+++ dev/aichi_front/aichi_sokuhou/front/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Front from './components/Front';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h2 className='container'>愛知県サッカー速報</h2>
+        <Front />
+      </header>
+    </div>
+  );
+}
+
+export default App;
